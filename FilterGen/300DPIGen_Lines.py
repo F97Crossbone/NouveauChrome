0e3b45c93adb9ed8e41f3c0e85e17c31b373a1a5
F97Crossbone <bdml350@gmail.com>
2025-04-11 17:45:31 +0200

    updated to working version

--- FilterGen/300DPIGen_Lines.py
+++ FilterGen/300DPIGen_Lines.py
@@ -8,13 +8,29 @@ from PIL import Image
 def main():
     """ Main program """
     #Generate array
-    colorArray = np.array([(255,0,0), (0,255,0), (0,0,255)])
-    DotArray = np.tile(colorArray, (474,397))#564060
-    #print(DotArray)
+    #colorArray = np.array([(255,0,0), (0,255,0), (0,0,255)])
+    img_w = 474
+    img_h = 397
+    LineArray = np.zeros((img_h, img_w, 3), dtype=np.uint8)
+    print(LineArray.shape)
+    j = 0
+    for i in range(397):
+        print(i)
+        if j == 3:
+            j = 0
+        match j:
+            case 0:
+                color = (250,0,0)
+            case 1:
+                color = (0,250,0)
+            case 2:
+                color = (0,0,250)
+        print(color)
+        LineArray[i] = color
+        j += 1
+    print(LineArray)
     #Generate image
-    #im = Image.new('RGB', (1190, 1422))
-    #im.putdata(DotArray)
-    im = Image.fromarray(DotArray, 'RGB').show()
-    im.save('testLines.png')
+    im = Image.fromarray(LineArray, 'RGB').show()
+    #im.save('testLines.png')
 if __name__ == "__main__":
     main()
